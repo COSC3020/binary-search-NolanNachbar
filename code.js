db0e85bce5244d0b4c2c3d4d92bda219a104b12d
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-08-27 22:01:33 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
-function binarySearch(list, element) {
-    return -1;
+function binarySearch(list, target) {
+    var l = 0; // Left bound
+    var r = list.length - 1; // Right bound
+
+    while (l <= r) {
+        var m = Math.floor((l + r) / 2); // The middle of the bounds
+
+        if (list[m] == target) {
+            return m;
+        }
+
+        if (list[m] < target) {
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+
+    return -1
 }
