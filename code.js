c98ddc4cd9ea1dab5eb8c72b6d9c12d7b34f4579
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-08-28 10:04:45 -0700

    Update code.js
    I certify that I have listed all sources used to complete this exercise, including the use
    of any Large Language Models. All of the work is my own, except where stated
    otherwise. I am aware that plagiarism carries severe penalties and that if plagiarism is
    suspected, charges may be filed against me without prior notice.
    
    I used this website for help with the syntax of the js code: https://www.w3schools.com/js/js_math.asp
    I have also worked on similar problems on leetcode so I already knew the basic structure that the answer would take.

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
 function binarySearch(list, element) {
-    return -1;
+    var l = 0; // Left bound
+    var r = list.length - 1; // Right bound
+
+    while (l <= r) {
+        var m = Math.floor((l + r) / 2); // The middle of the bounds
+
+        if (list[m] == element) {
+            return m;
+        }
+
+        if (list[m] < element) {
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+
+    return -1
 }
