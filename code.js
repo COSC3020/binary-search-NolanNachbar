85784c95c8fffa3aff4dd6be98e2b563cf4e7e33
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-08-28 20:40:39 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,12 @@
 function binarySearch(list, element) {
     let left = 0;
     let right = list.length - 1; 
-
+    
     while (left <= right) {
-        // Calculate middle index
+        // Calculate the middle index
         const middle = Math.floor((left + right) / 2); 
 
-        // Compare middle element with the target element 
+        // Compare the middle element with the target element 
         if (list[middle] === element) {
             return middle; 
         } else if (list[middle] < element) {
