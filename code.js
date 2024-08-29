66d4a4e09a01123804943323bf96291a8de633ff
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-08-28 20:53:36 -0600

    Update code.js
    I certify that I have listed all sources used to complete this exercise, including the use of any Large 
    Language Models. All of the work is my own, except where stated otherwise. I am aware that plagiarism carries severe penalties and that if plagiarism is suspected, charges may be filed against me without prior notice.
    I used this website for help with the syntax of the js code: https://www.w3schools.com/js/js_math.asp I have also worked on similar problems on leetcode so I already knew the basic structure that the answer would take.
    I also talked talked with Collin Davis and realized I needed to be using the === instead of ==.
    I ended up using the same variable types and names as him because although the logic of mine was the same, it wasn’t working.

--- code.js
+++ code.js
@@ -1,3 +1,19 @@
 function binarySearch(list, element) {
+    let left = 0;
+    let right = list.length - 1; 
+    
+    while (left <= right) {
+        // Calculate middle index
+        const middle = Math.floor((left + right) / 2); 
+
+        // Compare middle element with the target element 
+        if (list[middle] === element) {
+            return middle; 
+        } else if (list[middle] < element) {
+            left = middle + 1; 
+        } else {
+            right = middle -1; 
+        }
+    }
     return -1;
 }
